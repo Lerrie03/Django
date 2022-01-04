86ce3c151a5eea0495b3bb82533e9e6e282c913a
Lers03 <johnlerriechico3@gmail.com>
2022-01-04 14:06:23 +0800

    new commit

--- LerrieBSIT4B/djangoproj/urls.py
+++ LerrieBSIT4B/djangoproj/urls.py
@@ -1,4 +1,4 @@
-"""djangoproj URL Configuration
+"""LerrieBSIT4B URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
